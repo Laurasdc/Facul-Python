1d5d6c1da96de1dbb679bc55488372334b66b9cb
Laurasdc <laurasouzacarvalho82@gmail.com>
2024-09-04 23:06:40 -0300

    Acréscimo de mais exs na revisão

--- 2º Semestre/revisão cp4.py
+++ 2º Semestre/revisão cp4.py
@@ -1,33 +1,17 @@
-#Soma da matriz:
-'''def mostra_matriz(matriz): 
-    for linha in matriz:
-        print(linha)
-    return
+# 1 – Faça uma função que printa uma matriz da maneira convencional, ou seja, uma linha abaixo da outra:
+'''def imprimir_matriz(matriz):
+  for i in matriz:
+    for j in i:
+      print(j, end=" ")
+    print()
 
-def cria_matriz(linhas, colunas):
-    matriz = []
-    for i in range(linhas):
-        linha = []
-        for j in range(colunas):
-            valor = int(input(f"Digite o valor para a posição ({i+1},{j+1}): "))
-            linha.append(valor)
-        matriz.append(linha)
-    return matriz
+matriz = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
 
-def soma_matriz(matriz):
-    soma = 0  # Inicializa a soma como 0
-    for linha in matriz:
-        for elemento in linha:
-            soma += elemento  # Adiciona o elemento à soma total
-    return soma  # Retorna a soma total
-        
+imprimir_matriz(matriz)'''
 
-matriz = cria_matriz(2, 2)
-mostra_matriz(matriz)
-resultado = soma_matriz(matriz)
-print(f"A soma dos elementos da matriz é: {resultado}")'''
-
-#Matriz Transposta:
+# 2 - Faça uma função que recebe de parâmetros as dimensões (linhas e colunas) e retorna uma matriz preenchida de zeros com essas dimensões:
 '''def mostra_matriz(matriz):
     for linha in matriz:
         print(linha)
@@ -38,25 +22,14 @@ def cria_matriz(linhas, colunas):
     for i in range(linhas):
         linha = []
         for j in range(colunas):
-            valor = int(input(f"Digite o valor para a posição ({i+1},{j+1}): "))
-            linha.append(valor)
+            linha.append(0)
         matriz.append(linha)
     return matriz
 
-def matriz_transposta(matriz):
-    for i in range(len(matriz)):
-        for j in range(i):
-            aux = matriz[i][j]
-            matriz[i][j] = matriz[j][i]
-            matriz[j][i] = aux
-    return
-
-
-matriz = cria_matriz(3, 3)
-matriz_transposta(matriz)
+matriz = cria_matriz(3,3)
 mostra_matriz(matriz)'''
 
-#Mudar a diagonal para 1
+# 3 - Faça uma função que recebe uma matriz quadrada como parâmetro e altera todos os elementos da diagonal para 1:
 '''def mostra_matriz(matriz):
     for linha in matriz:
         print(linha)
@@ -76,11 +49,12 @@ def diagonal(matriz):
         matriz[i][i] = 1
     return
 
-matriz = cria_matriz(3,3)
+
+matriz = cria_matriz(5,5)
 diagonal(matriz)
 mostra_matriz(matriz)'''
 
-#Contra diagonal
+# 4 - Faça uma função que recebe uma matriz quadrada como parâmetro e altera todos os elementos da “contra-diagonal” para 1:
 '''def mostra_matriz(matriz):
     for linha in matriz:
         print(linha)
@@ -97,12 +71,147 @@ def cria_matriz(linhas, colunas):
 
 def contra_diagonal(matriz):
     for i in range(len(matriz)):
-        matriz[i][len(matriz) - 1 - i)] = 1
+        matriz[i][len(matriz) - 1 - i] = 1
     return
 
-matriz = cria_matriz(3,3)
+matriz = cria_matriz(5,5)
 contra_diagonal(matriz)
+mostra_matriz(matriz)   '''     
+
+# 5 - Faça uma função que recebe uma matriz quadrada e altera todos os elementos acima/abaixo da diagonal para 1:
+'''def mostra_matriz(matriz):
+    for linha in matriz:
+        print(linha)
+    return
+
+def cria_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
+#Abaixo da diagonal:
+def triognal_inferior(matriz):
+    for i in range(len(matriz)):
+        for j in range(i):
+            matriz[i][j] = 1
+    return''
+
+matriz = cria_matriz(5, 5)
+triognal_inferior(matriz)
+mostra_matriz(matriz)
+
+#Acima da diagonal:
+def triognal_superior(matriz):
+    for j in range(len(matriz)):
+        for i in range(j):
+            matriz[i][j] = 1
+    return
+
+matriz = cria_matriz(5, 5)
+triognal_superior(matriz)
 mostra_matriz(matriz)'''
+
+# 6 - Faça uma função que recebe uma matriz quadrada e retorne sua transposta: 
+'''def mostra_matriz(matriz):
+    for linha in matriz:
+        print(linha)
+    return
+
+    
+def cria_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
+    
+def triognal_inferior(matriz):
+    for i in range(len(matriz)):
+        for j in range(i):
+            matriz[i][j] = 1
+    return
+
+    
+def transporta(matriz):
+    for i in range(len(matriz)):
+        for j in range(i):
+            aux = matriz[i][j]
+            matriz[i][j] = matriz[j][i]
+            matriz[j][i] = aux
+    return
+
+    
+matriz = cria_matriz(10,10)
+triognal_inferior(matriz)
+mostra_matriz(matriz)'''
+
+# 8 - Faça um código que transforma uma matriz quadrada 8x8 em um tabuleiro de xadrez:
+'''def mostra_matriz(matriz):
+    for linha in matriz:
+        print(linha)
+    return
+
+    
+def cria_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)
+        matriz.append(linha)
+    return matriz
+
+
+xadrez = cria_matriz(8,8)
+for i in range(len(xadrez)):
+    for j in range(len(xadrez[i])):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                xadrez[i][j] = 1
+            else:
+                xadrez[i][j] = 0
+        else:
+            if j % 2 == 0:
+                xadrez[i][j] = 0
+            else:
+                xadrez[i][j] = 1
+
+mostra_matriz(xadrez)'''
+
+# 10 - Devido ao covid as cadeiras de cinema tem que ser utilizadas com um espaçamento de uma cadeira desocupada tanto na frente quanto atrás e dos lados. Represente está situação com uma matriz 50x50 em que cada local (i,j) tem nele a palavra “vaga” ou ocupada:
+'''def cria_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append("vaga")
+        matriz.append(linha)
+    return matriz
+
+
+def mostra_matriz(matriz):
+    for linha in matriz:
+        print(" ".join(linha))
+    return
+
+
+def ocupa_cadeiras(matriz,espacamento):
+    n = len(matriz)
+    for i in range(0, n, espacamento+1):  
+        for j in range(0, n, espacamento+1):
+            matriz[i][j] = "ocupada"
+
+
+matriz_cinema = cria_matriz(50, 50)
+ocupa_cadeiras(matriz_cinema, espacamento=1)
+mostra_matriz(matriz_cinema)'''
 
 #Matrizes com caso do vinho:
 def forca_num(msg):
